81fddd1d1ff1669be5cda73babd48b2a699a2595
Irene169 <55249916+Irene169@users.noreply.github.com>
2025-08-01 19:27:28 +0300

    Completed zoo simulator task

--- src/main/java/com/zoo/species/Elephant.java
+++ src/main/java/com/zoo/species/Elephant.java
@@ -1,4 +1,22 @@
 package com.zoo.species;
 
-public class Elephant {
+import com.zoo.animals.Mammal;
+
+public class Elephant extends Mammal{
+
+    // Constructor
+    public Elephant(String name, int age, double weight, int energyLevel, String furColor) {
+        super(name, age, weight, energyLevel, furColor);
+        this.furColor = furColor;
+    }
+    //Method override
+    @Override
+    public void makeSound () {
+        System.out.println(name + " says: Truba-bа-bа! ");
+    }
+// Adding a new method
+public void spraySelf() {
+        System.out.println(name + " is spraying intself with water from its trunk.  ");
+    }
 }
+
--- src/main/java/com/zoo/species/Lion.java
+++ src/main/java/com/zoo/species/Lion.java
@@ -1,4 +1,23 @@
 package com.zoo.species;
 
-public class Lion {
+import com.zoo.animals.Mammal;
+public class Lion extends Mammal {
+    // Constructor
+    public Lion(String name, int age, double weight, int energyLevel, String furColor) {
+    super(name, age, weight, energyLevel, furColor);
+}
+    //Method override
+    @Override
+    public void makeSound () {
+        System.out.println(name + " says: Rrrrr. ");
+    }
+    // Adding a new method
+    public void hunt () {
+        if (getEnergyLevel() >= 25) {
+            setEnergyLevel(getEnergyLevel() - 25);
+            System.out.println(name + " is hunting. Energy is decreased to " + getEnergyLevel());
+        } else {
+            System.out.println(name + " is too tired to hunt. ");
+        }
+    }
 }
